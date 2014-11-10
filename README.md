35bd7045a38f73aa4423fb0c95102fb4f0a3e492
cuish <cuihao0532@163.com>
2014-11-10 10:34:52 +0800

    modify readme, add a cpp file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-DesignPatternCPP
-================
+This is a readme file.
+
+The repo is a  set of .cpp which is design pattern with CPP.
+
+
---
+++ first.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "hello world, this is a demo!" << endl;
+    return 0;
+}
